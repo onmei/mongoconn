8b5c525bda4967a7386d9778c9adca163e9d9926
“YimZungWo” <ranfangxu@qq.com>
2023-01-04 21:17:44 +0800

    13

--- app/motor.py
+++ app/motor.py
@@ -1,10 +1,21 @@
+import asyncio
 import motor.motor_asyncio
 
-conn_str = "mongodb://mongo-0.mongo.mongo.svc.cluster.local:27017,mongo-1.mongo.mongo.svc.cluster.local:27017," \
-           "mongo-2.mongo.mongo.svc.cluster.local:27017/?replicaSet=rs0&authSource=db01&tls=true" \
-           "&tlsCertificateKeyFile=/rs0-client/dbuser01.pem&tlsCAFile=/rs0-ca/rs0-ca.pem "
-client = motor.motor_asyncio.AsyncIOMotorClient(conn_str)
 
-db = client.db01
+async def get_server_info() -> object:
 
-collections = db.users
+    conn_str = "mongodb://mongo-0.mongo.mongo.svc.cluster.local:27017,mongo-1.mongo.mongo.svc.cluster.local:27017," \
+               "mongo-2.mongo.mongo.svc.cluster.local:27017/?replicaSet=rs0&authSource=db01&tls=true" \
+               "&tlsCertificateKeyFile=/rs0-client/dbuser01.pem&tlsCAFile=/rs0-ca/rs0-ca.pem "
+    client = motor.motor_asyncio.AsyncIOMotorClient(conn_str, serverSelectionTimeoutMS=5000)
+    try:
+        print(await client.server_info())
+    except Exception:
+        print("Unable to connect to the server.")
+
+loop = asyncio.get_event_loop()
+
+loop.run_until_complete(get_server_info())
+# db = client.db01
+
+# collections = db.users
